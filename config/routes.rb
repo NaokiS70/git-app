9ac892d487a08259e32416d933b93f378e7ba668
NaokiS70 <naoki70sho@icloud.com>
2021-12-12 18:39:20 +0900

    フロント実装の準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'posts#index'
+  root to: "messages#index"
 end
 
 #編集中
